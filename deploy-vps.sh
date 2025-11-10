71920605e3c87a37562174e6cdb70684b1cd4659
Lazardeus <Laz.singh12@gmail.com>
2025-11-10 14:30:45 -0700

    chore(deploy): add cache-busting query params and nginx index.html fallback

--- deploy-vps.sh
+++ deploy-vps.sh
@@ -172,9 +172,9 @@ server {
         proxy_cache_bypass \$http_upgrade;
     }
 
-    # Static HTML files
+    # Static HTML files with index.html fallback
     location / {
-        try_files \$uri \$uri/ =404;
+        try_files \$uri \$uri/ /index.html;
     }
 }
 EOL
